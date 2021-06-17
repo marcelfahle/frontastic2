8ca619f784530393acefd901e103029543ff32c9
Marcel Fahle <m.fahle@gmail.com>
2021-06-17 12:38:35 +0200

    adds full bleed feature to cells

--- packages/frontastic.js/src/cell.tsx
+++ packages/frontastic.js/src/cell.tsx
@@ -5,12 +5,22 @@ export function Cell({
   size,
   children
 }: {
-  size: number;
+  size: number | "viewport";
   children: React.ReactNode;
 }) {
-  const styles: CSS.Properties = {
-    gridColumn: `span ${size} / span ${size}`,
-    overflow: "hidden"
-  };
+  let styles: CSS.Properties;
+  if (size === "viewport") {
+    styles = {
+      width: "100vw",
+      gridColumn: "span 12 / span 12",
+      marginLeft: "50%",
+      transform: "translateX(-50%)"
+    };
+  } else {
+    styles = {
+      gridColumn: `span ${size} / span ${size}`,
+      overflow: "hidden"
+    };
+  }
   return <div style={styles}>{children}</div>;
 }
